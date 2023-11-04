977b5033fab94132bf747cadb42c3784709b904a
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-11-04 16:40:04 +0300

    done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,6 +8,29 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+
+  const [isDeleting, setIsDeleting] = useState(false); // New state variable
+
+  const handleDeleteClick = () => {
+    // If a delete request is already in progress, do nothing
+    if (isDeleting) {
+      return;
+    }
+
+    // Disable the button and show a loading indicator
+    setIsDeleting(true);
+
+    // Call the onDelete function and pass the question ID for deletion
+    onDelete(id)
+      .then(() => {
+        // After a successful request, enable the button again
+        setIsDeleting(false);
+      })
+      .catch(() => {
+        // In case of an error, enable the button and handle the error
+        setIsDeleting(false);
+      });
+  };
 
   return (
     <li>
@@ -17,7 +40,12 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button
+        onClick={handleDeleteClick}
+        disabled={isDeleting}
+      >
+        {isDeleting ? "Deleting..." : "Delete Question"}
+      </button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Fetch questions from the server and update the state when the component mounts
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("Error fetching questions:", error));
+  }, []);
+
+  // Function to handle question deletion
+  const handleDeleteQuestion = (id) => {
+    // Make a DELETE request to the server to delete the question
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.status === 204) {
+          // If the deletion was successful on the server, update the state by removing the question
+          setQuestions((prevQuestions) =>
+            prevQuestions.filter((question) => question.id !== id)
+          );
+        } else {
+          console.error("Failed to delete question. Status code: " + response.status);
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting question:", error);
+      });
+  };
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDelete={handleDeleteQuestion}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
